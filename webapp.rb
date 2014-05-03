28bfe7963d5ace2f7f4b41a3ef2438a0d92ee6a7
Daniel Meißner <meise@koeln.ccc.de>
2014-05-03 22:47:05 +0200

    coding style: moved multiple filter definitions in separate function

--- webapp.rb
+++ webapp.rb
@@ -17,33 +17,32 @@ end
 # Formats
 get '/events.ical' do
   content_type 'text/calendar'
-  filter = { general: params[:filter], streaming: params[:streaming] }
-  cal = settings.parser.to_ical_calendar(settings.parser.filter(filter))
 
+  cal = settings.parser.to_ical_calendar(settings.parser.filter(filter_params))
   cal.to_ical
 end
 
 get '/events.atom' do
   content_type 'application/atom+xml'
 
-  filter = { general: params[:filter], streaming: params[:streaming] }
-  feed = settings.parser.to_atom(settings.parser.filter(filter))
-
+  feed = settings.parser.to_atom(settings.parser.filter(filter_params))
   feed.to_s
 end
 
 get '/events.txt' do
   content_type 'text/plain'
 
-  filter = { general: params[:filter], streaming: params[:streaming] }
-  settings.parser.to_txt(settings.parser.filter(filter))
+  settings.parser.to_txt(settings.parser.filter(filter_params))
 end
 
 get '/events.json' do
   content_type 'application/json'
 
-  filter = { general: params[:filter], streaming: params[:streaming] }
-  json = settings.parser.to_json(settings.parser.filter(filter))
-
+  json = settings.parser.to_json(settings.parser.filter(filter_params))
   json.to_s
+end
+
+# helper function
+def filter_params
+  { general: params[:filter], streaming: params[:streaming] }
 end
